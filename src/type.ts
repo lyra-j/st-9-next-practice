3e8daf21c43cc6724aa131b490dd9cc8e91155c2
lyra <mk.lyra.j@gmail.com>
2025-03-14 12:29:45 +0900

    🎉 4단계 완료

--- src/type.ts
+++ src/type.ts
@@ -12,3 +12,13 @@ export type News = {
   category: string;
   createdAt: number;
 };
+
+export type Blog={
+  id: string
+  title: string
+  content: string
+  author: string
+  category: string
+  createdAt: number
+  tags: string[]
+}
