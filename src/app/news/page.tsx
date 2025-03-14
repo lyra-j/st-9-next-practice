c9fb534812cbacf4008e280cafef76ba577eb3f3
lyra <mk.lyra.j@gmail.com>
2025-03-14 11:45:51 +0900

    🎉 1단계 완료

---
+++ src/app/news/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+
+ const NewsPage = () => {
+  return (
+    <div>News(ISR)</div>
+  )
+}
+
+export default NewsPage
---
+++ src/app/todolist/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TodoListPage() {
+  return <div>TodoList(CSR)</div>;
+}
+
+export default TodoListPage;
